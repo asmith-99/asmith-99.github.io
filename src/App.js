9a877848a0dfd3a1cf79c6697eb15edaf5372588
kaliesophic <kaliesophic@gmail.com>
2022-11-30 23:39:38 -0800

    lorem ipsum text, playing with image pattern bg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { LoremIpsum } from "lorem-ipsum";
 import React from "react";
 
 import styles from "./App.module.css";
+
+const lorem = new LoremIpsum();
 
 export default function App() {
   return (
@@ -12,9 +15,31 @@ export default function App() {
 }
 
 const PageFrame = () => {
-  return <div className={styles["page-frame"]}>Hello, world!</div>;
+  return (
+    <div className={styles["page-frame"]}>
+      <h1>Welcome</h1>
+      Hello, world!
+      <p>{lorem.generateParagraphs(1)}</p>
+      <p>{lorem.generateParagraphs(1)}</p>
+      <p>{lorem.generateParagraphs(1)}</p>
+      <p>{lorem.generateParagraphs(1)}</p>
+    </div>
+  );
 };
 
 const IndexFrame = () => {
-  return <div className={styles["index-frame"]}>Hello, world!</div>;
+  return (
+    <div className={styles["index-frame"]}>
+      <h2>Welcome</h2>
+      <hr />
+      <h2>Projects</h2>
+      <li> - Catbot</li>
+      <li> - This site</li>
+      <li> - akjshf</li>
+      <hr />
+      <h2>Welcome</h2>
+      <hr />
+      <h2>Welcome</h2>
+    </div>
+  );
 };
