314f8ddc9931d7bb7a7ff41efce402425c7c1ddd
kaliesophic <kaliesophic@gmail.com>
2022-11-30 22:56:55 -0800

    super basic structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import React from "react";
 
+import styles from "./App.module.css";
+
 export default function App() {
-  return <div>Hello, world!</div>;
+  return (
+    <div className={styles["app-container"]}>
+      <IndexFrame />
+      <PageFrame />
+    </div>
+  );
 }
+
+const PageFrame = () => {
+  return <div className={styles["page-frame"]}>Hello, world!</div>;
+};
+
+const IndexFrame = () => {
+  return <div className={styles["index-frame"]}>Hello, world!</div>;
+};
