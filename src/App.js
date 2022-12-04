7582e63a7f689f9c6d224eb8785ba017ae600029
kaliesophic <kaliesophic@gmail.com>
2022-12-04 05:46:45 -0800

    formatting improvement, seeded RNG for the lorem ipsum generator

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import { LoremIpsum } from "lorem-ipsum";
 import React from "react";
+import seedrandom from "seedrandom";
 
 import styles from "./App.module.css";
 
-const lorem = new LoremIpsum();
+const seededRNG = seedrandom("a seed for the rng.");
+
+const lorem = new LoremIpsum({
+  random: seededRNG,
+});
 
 export default function App() {
   return (
     <div className={styles["app-container"]}>
-      <IndexFrame />
-      <PageFrame />
+      <div className={styles["index-container"]}>
+        <Index />
+      </div>
+      <div className={styles["pages-container"]}>
+        <Pages />
+      </div>
     </div>
   );
 }
 
-const PageFrame = () => {
+// the place where one or more pages are kept
+const Pages = () => {
+  return <Page />;
+};
+
+const Page = () => {
   return (
-    <div className={styles["page-frame"]}>
+    <div className={styles["page"]}>
       <h1>Welcome</h1>
       Hello, world!
       <p>{lorem.generateParagraphs(1)}</p>
@@ -27,9 +41,9 @@ const PageFrame = () => {
   );
 };
 
-const IndexFrame = () => {
+const Index = () => {
   return (
-    <div className={styles["index-frame"]}>
+    <div className={styles["index"]}>
       <h2>Welcome</h2>
       <hr />
       <h2>Projects</h2>
