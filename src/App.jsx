a7bbad2b161eb12eed5f09205c59455908ba60ad
asmith-99 <aiden.smith10@gmail.com>
2025-03-02 00:47:57 -0800

    basic formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="container"> Hello, world!</div>;
+  return (
+    <div className="central">
+      <header className="header-bar">Aiden Smith</header>
+      <section>
+        Hi! Here's an attempt at a portfolio site. Naturally, I'm immediately
+        overcomplicating things.
+      </section>
+      <small>
+        Here's a sidenote. I can make this as long as I want, and it'll fit just
+        fine!
+      </small>
+      <section>And here's another section of text.</section>
+    </div>
+  );
 }
 
 export default App;
