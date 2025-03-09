2a67f161620333fa154433b06f387e8b894c1d13
asmith-99 <aiden.smith10@gmail.com>
2025-03-08 23:26:35 -0800

    basic embedded resume page

--- src/ResumePage/ResumePage.jsx
+++ src/ResumePage/ResumePage.jsx
@@ -1,5 +1,28 @@
 import styles from "./ResumePage.module.scss";
 
 export default function ResumePage() {
-  return <>Resume goes here!</>;
+  return (
+    <div className={styles["content"]}>
+      <div>
+        Want to download a copy? Click{" "}
+        <a href="/Resume draft 2.pdf" download="Aiden Smith.pdf">
+          here
+        </a>
+      </div>
+      <object
+        className={styles["embedded-resume"]}
+        data="/Resume draft 2.pdf"
+        type="application/pdf"
+        standby="hello? this thing on?"
+      >
+        <p>
+          Sorry, it looks like your browser does not support inline PDFs. Please
+          <a href="/Resume draft 2.pdf" download="Aiden Smith.pdf">
+            download the PDF
+          </a>
+          .
+        </p>
+      </object>
+    </div>
+  );
 }
