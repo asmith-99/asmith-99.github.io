11abe18297ff5609afac298d46e3fe94002781d2
asmith-99 <aiden.smith10@gmail.com>
2025-03-11 23:30:00 -0700

    added project blurbs, style and functionality tweaks

--- src/ProjectsPage/Project.jsx
+++ src/ProjectsPage/Project.jsx
@@ -1,23 +1,41 @@
+import { Link } from "react-router";
 import styles from "./Project.module.scss";
+import { useState } from "react";
 
-export default function Project({ name, duration, description, blurb, img }) {
+export default function Project({
+  name,
+  duration,
+  description,
+  blurb,
+  coverImage,
+  writeupLink,
+}) {
+  const [showMore, setShowMore] = useState(false);
+
   return (
     <div className={styles["container"]}>
       <div className={styles["preview-content"]}>
-        <img className={styles["cover-image"]} src={img} />
+        <img className={styles["cover-image"]} src={coverImage} />
         <div className={styles["details"]}>
           <div className={styles["project-name"]}>
             <h3>{name}</h3>
             <span className={styles["duration"]}>{duration}</span>
           </div>
-          {blurb}
-          <span className={styles["more-prompt"]}>More...</span>
+          <div className={styles["blurb"]}>{blurb}</div>
         </div>
       </div>
       <div className={styles["more-content"]}>
         {description.map((entry) => (
           <p>{entry}</p>
         ))}
+      </div>
+      <div className={styles["button-container"]}>
+        {writeupLink && (
+          <Link to={writeupLink} className={styles["writeup-link"]}>
+            Writeup
+          </Link>
+        )}
+        <span className={styles["more-prompt"]}>More...</span>
       </div>
     </div>
   );
--- src/ProjectsPage/ProjectsPage.jsx
+++ src/ProjectsPage/ProjectsPage.jsx
@@ -3,6 +3,7 @@ import resume from "../resume.json";
 import styles from "./ProjectsPage.module.scss";
 
 export default function ProjectsPage() {
+  console.log(resume.projects[0]);
   const academicProjects = resume.projects.filter(
     (project) => project.isAcademic
   );
@@ -14,18 +15,19 @@ export default function ProjectsPage() {
       <h1>Personal Projects</h1>
       <p>
         These are projects which I completed on my own time for my own personal
-        interest, or to solve a problem I had. Hover over them to find out more!
+        interest, or to solve a problem I had. Hover over them or click
+        'More...'
       </p>
       {personalProjects.map((projectDetails) => (
-        <Project {...projectDetails} />
+        <Project {...projectDetails} key={projectDetails.name} />
       ))}
       <h1>Academic Projects</h1>
       <p>
         These are projects which I completed as a part of my education. Hover
-        over them to find out more!
+        over them or click 'More...'
       </p>
       {academicProjects.map((projectDetails) => (
-        <Project {...projectDetails} />
+        <Project {...projectDetails} key={projectDetails.name} />
       ))}
     </div>
   );
