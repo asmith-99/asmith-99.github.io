963d5e9e53a0682e384aea3e854ba0b1d87f2550
asmith-99 <aiden.smith10@gmail.com>
2025-03-16 15:59:03 -0700

    minor css tweaks

--- src/StarsPage/StarsPage.jsx
+++ src/StarsPage/StarsPage.jsx
@@ -6,7 +6,8 @@ export default function StarsPage() {
       The background for this site was written using three.js, and shows a view
       of the 9000 brightest stars. The size and color of the stars represents
       the apparent magnitude and color index of the real star. On this page the
-      background is clickable- try clicking and dragging, or zooming out!
+      background is clickable (below this paragraph). Try clicking and dragging,
+      or zooming in and out!
       <p>
         Want to know more? Check out the{" "}
         <a href="https://github.com/asmith-99/asmith-99.github.io">
